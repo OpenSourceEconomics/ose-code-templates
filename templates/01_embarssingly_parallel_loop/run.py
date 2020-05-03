72b1e47c6d8b1ac4de94cdd0c1cc72c1a9282acb
peisenha <peisenha@protonmail.com>
2020-05-03 13:06:48 +0200

    edits

--- templates/01_embarssingly_parallel_loop/run.py
+++ templates/01_embarssingly_parallel_loop/run.py
@@ -20,30 +20,41 @@ There a lot of supporting material available online.
 Examples
 -------
 
-$ mpiexec.mpich -n 1 -usize 3 python run.py
+$ mpiexec -n 1 -usize 3 python run.py
 
 $ python run.py
 
 """
+from functools import partial
 
-import time
+import numpy as np
 
 from core_functions import distribute_tasks
 
-#
-def example_task(task, **kwargs):
 
-    time.sleep(3)
+def example_task(alpha, beta, gamma, x):
 
-    return task
+    x_ = np.array(x)
+    result = alpha * (x_[1:] - x_[:-1] ** beta) ** gamma + (1 - x_[:-1]) ** gamma
 
+    return result.sum()
 
-# black codecov
 
 if __name__ == "__main__":
 
-    tasks = range(5)
+    # We fix the details of our evaluation task and draw a sample of
+    # evaluation points.
+    num_points = 300
+    num_inputs = 3
 
-    rslt = distribute_tasks(example_task, tasks, num_cores=3, distributed=True)
+    total_draws = num_inputs * num_points
+    eval_points = np.random.uniform(size=total_draws).reshape(num_points, num_inputs)
 
-    # clickup
+    # We partial out all function arguments that remain unchanged.
+    alpha, beta, gamma = 100.0, 2.0, 3.0
+    p_example_task = partial(example_task, alpha, beta, gamma)
+
+    # We specify the amount and type of resources we have available and then
+    # start the parallel evaluation of our test function.
+    num_cores, is_distributed = 3, True
+    distribute_tasks(p_example_task, eval_points, num_cores, is_distributed)
