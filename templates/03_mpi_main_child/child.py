90d049d5212180ec9a05a2c3e0f0b8498aed88f6
peisenha <peisenha@protonmail.com>
2020-08-12 10:31:57 +0200

    even better version

--- templates/03_mpi_main_child/child.py
+++ templates/03_mpi_main_child/child.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 """This script provides all capabilities for the child processes."""
-import pickle as pkl
-import shutil
-import glob
+
 import os
+
+# In this script we only have explicit use of MPI as our level of parallelism. This needs to be
+# done right at the beginning of the script.
+update = {'NUMBA_NUM_THREADS': '1', 'OMP_NUM_THREADS': '1', 'OPENBLAS_NUM_THREADS': '1',
+          'NUMEXPR_NUM_THREADS': '1', 'MKL_NUM_THREADS': '1'}
+os.environ.update(update)
 
 import pandas as pd
 import numpy as np
@@ -14,6 +18,9 @@ from mpi4py import MPI
 comm = MPI.Comm.Get_parent()
 num_slaves, rank = comm.Get_size(), comm.Get_rank()
 
+# We need some additional task-specific information.
+prob_info = comm.bcast(None, root=0)
+
 subdir = f"subdir_child_{rank}"
 os.mkdir(subdir)
 os.chdir(subdir)
@@ -22,37 +29,37 @@ os.chdir(subdir)
 params, options, df = rp.get_example_model("robinson_crusoe_basic")
 simulate = rp.get_simulate_func(params, options)
 
-
-prob_info = comm.bcast(None, root=0)
-
 rslt = list()
 while True:
-    # Signal readiness
-    comm.Send([np.zeros(1, dtype="float64"), MPI.DOUBLE], dest=0)
 
+    # Signal readiness
+    comm.Send([np.zeros(1, dtype="int64"), MPI.DOUBLE], dest=0)
+
+    # Receive instructions and act accordingly.
     cmd = np.array(0, dtype="int64")
     comm.Recv([cmd, MPI.INT], source=0)
 
-    #
     if cmd == 0:
-        print("done")
+        # We set up a container to store the results.
+        df = pd.DataFrame(rslt, columns=["qoi", "delta", "exp_edu"])
+        df.index.name = "sample"
+        df.to_pickle(f"rslt_child_{rank}.pkl")
+
+        # Now we are ready to acknowledge completion and disconnect.
+        comm.Send([np.zeros(1, dtype="int64"), MPI.DOUBLE], dest=0)
         comm.Disconnect()
         break
 
     elif cmd == 1:
         # We are called to sample the quantity of interest and need to update the parameters
         # accordingly.
-        sample = np.array([0.0, 0.0], dtype="float64")
+        sample = np.empty(prob_info["num_params"], dtype="float64")
         comm.Recv([sample, MPI.DOUBLE], source=0)
 
         # params.loc["delta", "value"], params.loc[("wage_a", "exp_edu"), "value"] = sample
         params.loc["delta", "value"], params.loc[("wage_fishing", "exp_fishing"), "value"] = sample
         stat = simulate(params).groupby("Identifier")["Experience_Fishing"].max().mean()
         rslt.append([stat, *sample])
+
     else:
         raise AssertionError
-
-# We set up a container to store the results.
-df = pd.DataFrame(rslt, columns=["qoi", "delta", "exp_edu"])
-df.index.name = "sample"
-df.to_pickle(f"rslt_child_{rank}.pkl")
--- templates/03_mpi_main_child/main.py
+++ templates/03_mpi_main_child/main.py
@@ -3,26 +3,19 @@ import shutil
 import sys
 import os
 
-# In this script we only have explicit use of MPI as our level of parallelism. This needs to be
-# done right at the beginning of the script.
-update = {'NUMBA_NUM_THREADS': '1', 'OMP_NUM_THREADS': '1', 'OPENBLAS_NUM_THREADS': '1',
-          'NUMEXPR_NUM_THREADS': '1', 'MKL_NUM_THREADS': '1'}
-os.environ.update(update)
-
 import numpy as np
 import pandas as pd
+import chaospy as cp
+
+if 'PMI_SIZE' not in os.environ.keys():
+    raise AssertionError("... requires MPI")
 
 from mpi4py import MPI
 
-
-import chaospy as cp
-
 status = MPI.Status()
 
-num_sample = 4
+num_sample = 5
 num_child = 2
-
-# TODO: Special case where num_sample = num_rprocs
 
 """We create our samples here as a numpy array. This is the fastest alternative"""
 distribution = cp.J(cp.Uniform(0.92, 0.98), cp.Uniform(0.03, 0.08))
@@ -33,7 +26,6 @@ num_params = samples.shape[1]
 info = MPI.Info.Create()
 info.update({"wdir": os.getcwd()})
 
-
 # We need all child processes to work in a separate directory.
 [shutil.rmtree(dirname) for dirname in glob.glob("subdir_child_*")]
 file_ = os.path.dirname(os.path.realpath(__file__)) + '/child.py'
@@ -42,17 +34,14 @@ comm = MPI.COMM_SELF.Spawn(sys.executable, args=[file_], maxprocs=num_child, inf
 # We send all problem-specific information once and for all.
 prob_info = dict()
 prob_info["num_params"] = num_params
-prob_info["num_sample"] = num_sample
 comm.bcast(prob_info, root=MPI.ROOT)
 
-check_in = np.zeros(1, dtype='float64')
 cmd = dict()
 cmd['terminate'] = np.array(0, dtype='int64')
 cmd['execute'] = np.array(1, dtype='int64')
+check_in = np.zeros(1, dtype='int64')
 
 for sample in samples:
-
-    sample = np.array(sample, ndmin=2)
 
     comm.Recv([check_in, MPI.DOUBLE], status=status)
     rank_sender = status.Get_source()
@@ -63,8 +52,10 @@ for sample in samples:
     comm.Send([sample, MPI.DOUBLE], dest=rank_sender)
 
 # We are done and now terminate all child processes properly and finally the turn off the
-# communicator.
+# communicator. We need for all to acknowledge the receipt to make sure we do not continue here
+# before all tasks are not only started but actually finished.
 [comm.Send([cmd['terminate'], MPI.INT], dest=rank) for rank in range(num_child)]
+[comm.Recv([check_in, MPI.DOUBLE], status=status) for rank in range(num_child)]
 comm.Disconnect()
 
 # We aggregate all dataframes into a single file.
@@ -74,5 +65,9 @@ for subdir in glob.glob("subdir_child_*"):
     df = pd.read_pickle(f"{subdir}/rslt_child_{rank}.pkl")
     df["rank"] = rank
     dfs.append(df)
-rslt = pd.concat(dfs, axis=0).reset_index()
+rslt = pd.concat(dfs, axis=0, ignore_index=True)
+rslt.index.name ="sample"
 rslt.to_pickle("rslt_main.pkl")
+[shutil.rmtree(dirname) for dirname in glob.glob("subdir_child_*")]
+
+print(rslt)
